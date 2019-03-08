94d0a8145cfb6197742be5f3018adbfa4e5ba05e
shixj@inhand.com.cn <shixj@inhand.com.cn>
2019-03-08 16:35:42 +0800

    first commit

---
+++ pages/login/login.js
@@ -0,0 +1,94 @@
+// pages/login/login.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+     disabled:true,
+     btnstate:"default",
+     account:"",
+     password:""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  accountInput:function(e){
+    var content = e.detail.value;
+    if (content!=""){
+      this.setData({
+        disabled: false,
+        btnstate: "primary",
+        account: content
+      });
+    }else{
+      this.setData({
+         disabled: true,
+         btnstate: "default"
+      });
+    }
+  },
+  pwdBlur:function(e){
+    var password = e.detail.value;
+    if (password!=''){
+       this.setData({
+         password: password
+       });
+    }
+  },
+  login:function(){
+
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
